0e9375ccf719102d9f51b583cf8f42e91bc2a85a
sholl <ogunleoa@mail.uc.edu>
2025-07-21 03:49:38 -0400

    updates

--- Lab_04_Java_Variables/src/Main.java
+++ Lab_04_Java_Variables/src/Main.java
@@ -1,16 +1,129 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Integer variables
+        int intOperandA = 5;
+        int intOperandB = 13;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+        // Double variables
+        double doubleOperandA = 7.5;
+        double doubleOperandB = 3.2;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        // Integer arithmetic operations
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of " + intOperandA + " divided by " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder (modulo) using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        // Double arithmetic operations
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of " + doubleOperandA + " divided by " + doubleOperandB + " is " + doubleQuotient);
+
+        // Declarations
+        int numberOfKidsInFamily = 3;
+        boolean isItRaining = false;
+        double priceOfGallonGas = 3.79;
+        int favoriteNumber = 7;
+        double shoeSize = 10.5;
+        String birthMonth = "March";
+        String fullName = "Shola Ogunleye";
+
+        // Statements
+        System.out.println("Number of kids in my family: " + numberOfKidsInFamily);
+        System.out.println("Is it raining? " + isItRaining);
+        System.out.println("Price of a gallon of gas: $" + priceOfGallonGas);
+        System.out.println("My favorite number: " + favoriteNumber);
+        System.out.println("My shoe size: " + shoeSize);
+        System.out.println("My birth month: " + birthMonth);
+        System.out.println("My full name: " + fullName);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
